0dddf7b4ef0b4abf7b4d607505f80dce5a8575d3
mdroberts74 <mikedroberts88@gmail.com>
2018-05-22 15:32:16 -0500

    I added the JavaScript in the main.js file

---
+++ main.js
@@ -0,0 +1,16 @@
+const article = document.querySelector("#congressional");
+console.log(article)
+
+let paragraphElement = document.createElement("p")
+let txt = document.createTextNode("Blackburn is shown as a purple triangle in our ideology-leadership chart below. Each dot is a member of the House of Representatives positioned according to our liberal–conservative ideology score left to right and our leadership score ");
+para.appendChild(txt);
+console.log("para")
+
+article.appendChild(paragraphElement)
+
+
+
+/*
+
+const congressionalDistAttr = document.createAttribute(congressional - district)
+*/
